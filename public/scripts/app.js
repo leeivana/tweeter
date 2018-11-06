a4c621261af782dc2abeade70147626f5c837c77
leeivana <lee.ivana@hotmail.com>
2018-11-06 14:32:23 -0500

    Implemented a createTweetElement function that returns the html structure of a tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,57 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+
+
+
+$(document).ready(function(){
+  // $('#submit').on('click', createTweetElement(){
+  // });
+
+    const createTweetElement = () =>{
+    const name = tweetData.user.name;
+    const avatar = tweetData.user.avatars.small;
+    const userID = tweetData.user.handle;
+    const tweet = tweetData.content.text;
+    const time = tweetData['created_at'];
+    const $tweet = $('<article>').attr('class', 'tweet').appendTo('#list');
+    $('<header>').attr('class', 'head').appendTo($tweet);
+      $(`<img src='${avatar}'>`).appendTo('.head');
+      $('<h2>').text(name).attr('class', 'name').appendTo('.head');
+      $('<p>').text(userID).attr('class', 'userID').appendTo('.head');
+    $('<p>').text(tweet).attr('class', 'tweet-content').appendTo($tweet);
+    $('<footer>').attr('class', 'foot').appendTo($tweet);
+      $('<p>').text(time).attr('class', 'date').appendTo('.foot');
+      $('<div>').attr('class', 'icons').appendTo('.foot');
+        $('<i>').attr('class', 'fas fa-flag').appendTo('.icons');
+        $('<i>').attr('class', 'fas fa-retweet').appendTo('.icons');
+        $('<i>').attr('class', 'fas fa-heart').appendTo('.icons');
+    console.log($tweet);
+    console.log('hi');
+    return $tweet;
+  }
+  createTweetElement();
+});
+
+
+
+
+// createTweetElement(tweetData);
+
+// var $tweet = $("<article>").addClass("tweet");
